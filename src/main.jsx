c7f35e0d08f0d7e2c1c3ec91b5e46b273c2b2c46
KUSHARGAVARMA <kushavrm008@gmail.com>
2024-08-20 19:25:45 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 
 createRoot(document.getElementById('root')).render(
-    
- 
      <Provider store={store}>
     <App />
     </Provider>
